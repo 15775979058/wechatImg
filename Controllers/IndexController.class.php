0bad0ecea3fd0f64e96f3305984bf331bbfbe840
Greatfar <panggreatfar@foxmail.com>
2017-03-14 09:19:06 +0800

    增加首页微信js-sdk应用

--- Controllers/IndexController.class.php
+++ Controllers/IndexController.class.php
@@ -7,8 +7,16 @@ class IndexController {
         
     }
     
-    //上传表单
+    //首页
     function indexAction() {
+        //检查是否已经登录
+        require './Models/UserModel.class.php';
+        $user = new UserModel();
+        $user->loginCheck(false);
+        //调用JS-SDK
+        require_once './Models/JsSdkModel.class.php';
+        $jssdk = new JSSDK("wx011cddc56212c6ed", "b2aa40a0d1f71b9ba4103ca05c22453e");
+        $signPackage = $jssdk->GetSignPackage();
         //载入首页视图
         require_once './Views/index.html';
     }
